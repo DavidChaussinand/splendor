59cc96260c8597efaf192e12c5ee66afd047770b
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-10-25 14:40:53 +0200

    #2 réfacto take 3 token and decrease stock board

--- engine/TakeDifferentColorToken.py
+++ engine/TakeDifferentColorToken.py
@@ -2,9 +2,13 @@ class TakeDifferentColorToken:
     def __init__(self, game_repository):
         self.game_repository = game_repository
 
-    def execute(self, player_number):
+    def execute(self, player_number, blue, green, red, black, white):
         game = self.game_repository.get_game()
-        game.player_list[player_number].red = 1
-        game.player_list[player_number].green = 1
-        game.player_list[player_number].black = 1
+        tokens = [("red", red), ("blue", blue), ("green", green), ("black", black), ("white", white), ]
+
+        for (color, taken) in tokens:
+            if taken:
+                game.board.stock.decrease(color, quantity=1)
+                game.player_list[player_number].stock.increase(color, quantity=1)
+
         self.game_repository.save_game(game)
--- test/TestTakeToken.py
+++ test/TestTakeToken.py
@@ -1,27 +1,49 @@
+from pprint import pprint
+
 import pytest
 
 from engine.GameStart import GameStart
 from engine.TakeDifferentColorToken import TakeDifferentColorToken
+from model.Board import Board
+from model.Game import Game
 from model.Player import Player
+from model.Stock import Stock
 from repository.GameRepositoryInMemory import GameRepositoryInMemory
 
 @pytest.fixture
 def game_init():
     game_repository = GameRepositoryInMemory()
     game_start = GameStart(game_repository)
-    game_start.execute(3)
+    game_start.execute(2)
     return game_repository
+
+
+def create_stock(quantity):
+    return Stock(quantity, quantity, quantity, quantity, quantity)
+
 
 def test_take_different_color_token (game_init):
     player_number = 0
     take_different_color_token = TakeDifferentColorToken(game_init)
-    take_different_color_token.execute(player_number)
-    game = game_init.get_game()
-    player = Player()
-    player.red=1
-    player.green=1
-    player.black=1
-    expected = list[player.red, player.green, player.black]
-    actual = list[game.player_list[player_number].red,game.player_list[player_number].green,game.player_list[player_number].black]
+    take_different_color_token.execute(player_number, blue=True, green=True, white=True, black=False, red=False)
+    expected = (Game(Board(yellow=5,
+                           stock=Stock(red=4, green=3, black=4, white=3, blue=3),
+                           card_level1=4,
+                           card_level2=4,
+                           card_level3=4,
+                           noble_number=3,
+                           cards=[]),
+                     player_list=[Player(), Player()]),
+                1,
+                1,
+                1,
+                0,
+                0)
+    actual = (game_init.get_game(),
+              game_init.get_game().player_list[0].stock.blue,
+              game_init.get_game().player_list[0].stock.green,
+              game_init.get_game().player_list[0].stock.white,
+              game_init.get_game().player_list[0].stock.black,
+              game_init.get_game().player_list[0].stock.red)
 
     assert actual == expected
