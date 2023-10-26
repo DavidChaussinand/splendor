0849dad6347243f806e1865ef0df7deaab3d9415
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-10-26 16:27:01 +0200

    #6 réfacto

--- engine/BuyCard.py
+++ engine/BuyCard.py
@@ -8,19 +8,18 @@ class BuyCard:
     def execute(self, player_number, card_number):
         game = self.game_repository.get_game()
         card = game.board.cards[card_number]
-        game.player_list[player_number].cards.append(card)
+        player = game.player_list[player_number]
+        player.cards.append(card)
         del game.board.cards[card_number]
 
         for color in card.cost.__dict__:
-            game.player_list[player_number].stock.decrease(color, card.cost.__dict__[color])
+            player.stock.decrease(color, card.cost.__dict__[color])
+            game.board.stock.increase(color, card.cost.__dict__[color])
 
-        enough_tokens = True
-        for value in game.player_list[player_number].stock.__dict__.values():
+        for value in player.stock.__dict__.values():
             if value < 0:
-                enough_tokens = False
+                raise ValueError('Not enough tokens')
 
-        if enough_tokens:
-            self.game_repository.save_game(game)
-        else:
-            raise ValueError('Not enough tokens')
+        self.game_repository.save_game(game)
+
 
