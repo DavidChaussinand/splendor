fa060fda1aa70d2e3b08f913d49e613863ae3c85
Chaussinand <lebib07@live.fr>
2023-10-30 14:22:22 +0100

    ajout vue prendre différent jetons

--- webgame/urls.py
+++ webgame/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from webgame import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("take_different_tokens/<int:player_number>", views.take_different_tokens, name="take_different_tokens")
 ]
--- webgame/views.py
+++ webgame/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from engine.GameStart import GameStart
+from engine.Query import Query
+from engine.TakeDifferentColorToken import TakeDifferentColorToken
+from repository.GameRepositoryInMemory import GameRepositoryInMemory
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world")
+
+
+def take_different_tokens(request, player_number):
+    game_repository = GameRepositoryInMemory()
+    game_start = GameStart(game_repository)
+    query = Query(game_repository)
+    take_different_color_token = TakeDifferentColorToken(game_repository)
+
+    game_start.execute(2)
+    game_state = query.show_game_state()
+
+    return HttpResponse("prend tes jetons connard : le joueur est le numéro " + str(player_number))
