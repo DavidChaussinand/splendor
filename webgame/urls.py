383c552e1795f11f58579e8a09536aea4edd06ce
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-10-30 14:21:48 +0100

    print gamelauncher state

--- webgame/urls.py
+++ webgame/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from webgame import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("gamelauncher", views.game_launcher, name="launchgame")
 ]
--- webgame/views.py
+++ webgame/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from engine.GameStart import GameStart
+from repository.GameRepositoryInMemory import GameRepositoryInMemory
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world")
+
+def game_launcher(request):
+    game_repository = GameRepositoryInMemory()
+    GameStart(game_repository).execute(number_of_player=2)
+    return HttpResponse(str(game_repository.get_game()))
