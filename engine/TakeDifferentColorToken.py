32ca10b06f735e07fccac72b97cb707ea81122ca
Chaussinand <lebib07@live.fr>
2023-10-31 11:54:16 +0100

    template prendre un jeton

--- engine/TakeDifferentColorToken.py
+++ engine/TakeDifferentColorToken.py
@@ -6,6 +6,10 @@ class TakeDifferentColorToken:
         game = self.game_repository.get_game()
         tokens = [("red", red), ("blue", blue), ("green", green), ("black", black), ("white", white), ]
         counter = 0
+        available_colors = 0
+        for x in game.board.stock.__dict__:
+            if game.board.stock.__dict__[x] > 0:
+                available_colors += 1
         for (color, taken) in tokens:
             if taken:
                 counter += 1
@@ -14,6 +18,6 @@ class TakeDifferentColorToken:
         for x in game.board.stock.__dict__:
             if game.board.stock.__dict__[x] < 0:
                 raise ValueError(f'No {x} tokens on board')
-        if counter == 0 or counter > 3:
+        if (counter < 3 and available_colors > counter) or counter > 3:
             raise ValueError('not right amount of token taken')
         self.game_repository.save_game(game)
--- webgame/views.py
+++ webgame/views.py
@@ -27,10 +27,7 @@ def board(request):
 
 
 def take_different_tokens(request, player_number):
-    # game launch
-    game_start = GameStart(game_repository)
     query = Query(game_repository)
-    game_start.execute(2)
     game_state = query.show_game_state()
 
     # if this is a POST request we need to process the form data
@@ -39,6 +36,8 @@ def take_different_tokens(request, player_number):
         form = TokenForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
+            take_different_token = TakeDifferentColorToken(game_repository)
+            take_different_token.execute(0, blue=form.cleaned_data["blue"], black=form.cleaned_data["black"], white=form.cleaned_data["white"], red= form.cleaned_data["red"], green=form.cleaned_data["green"])
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
@@ -51,8 +50,6 @@ def take_different_tokens(request, player_number):
         for color in game_state.board.stock.__dict__:
             if game_state.board.stock.__dict__[color] <= 0:
                 form.fields.pop(color)
-
-        form.fields.pop('red')
 
     return render(request, "webgame/take_different_tokens.html", {"tokenform": form})
 
